f6b71e0c38eb75b315f92b8bc2a426692fb8cd09
Future Inventors Lab <153513974+futureinv@users.noreply.github.com>
2026-05-29 15:27:10 +0200

    Add files via upload

---
+++ AVVIA_SERVER.bat
@@ -0,0 +1,5 @@
+@echo off
+echo Avvio del server di tracking in corso...
+echo NON CHIUDERE questa finestra, lasciala aperta in background.
+node server3.js
+pause
---
+++ AVVIA_SERVER_sgrok.bat
@@ -0,0 +1,21 @@
+@echo off
+echo Avvio del sistema in corso...
+
+:: 1. Apre una finestra separata per il Server Node
+:: Il "title" serve per identificarla facilmente
+start "SERVER_TUIO" cmd /k "node server4.js"
+
+:: 2. Breve attesa per permettere al server di inizializzare la porta 8080
+timeout /t 2
+
+:: 3. Apre una finestra separata per Ngrok
+:: Rimarrà aperta così puoi copiare l'indirizzo con calma
+start "NGROK_TUNNEL" cmd /k "ngrok http 8080"
+
+echo.
+echo ========================================================
+echo Finito!
+echo - Nella finestra 'SERVER_TUIO' vedrai i tuoi console.log.
+echo - Nella finestra 'NGROK_TUNNEL' vedrai l'indirizzo HTTPS.
+echo ========================================================
+pause
